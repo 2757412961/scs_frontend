1ee52a82f61cf3e6b89e6000b28669ed1cd391c0
2757412961 <39891566+2757412961@users.noreply.github.com>
2020-10-06 16:16:45 +0800

    天气现象

---
+++ src/components/NumericalPrediction/PHProductImgs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Button type="primary" @click="modal1 = true">Display dialog box</Button>
+    <Modal
+      v-model="modal1"
+      title="Common Modal dialog box title"
+      @on-ok="ok"
+      @on-cancel="cancel">
+      <p>Content of dialog</p>
+      <p>Content of dialog</p>
+      <p>Content of dialog</p>
+    </Modal>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "PHProductImgs",
+        data: function () {
+            return {
+                modal1: false
+
+
+            }
+        },
+        methods: {
+            ok() {
+                this.$Message.info('Clicked ok');
+            },
+            cancel() {
+                this.$Message.info('Clicked cancel');
+            }
+
+
+        },
+        created() {
+            console.log("PHProductImgs is created");
+        },
+        mounted() {
+            console.log("PHProductImgs is created");
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -14,55 +14,50 @@
 <script>
     import MapLayout from "./MapLayout";
     import HeadNav from "./HeadNav";
-    import Typhoon from "../components/Typhoon/Typhoon";
-
-    import {globalBus} from "../components/globalBus";
 
     export default {
-      name: "home",
-      components: {Typhoon, HeadNav, MapLayout},
-      computed: {
-
-      },
+        name: "home",
+        components: {HeadNav, MapLayout},
+        computed: {},
         methods: {
-          clearMap(val){
-              this.$refs.mapLayout.clearLayerSource();
-          }
+            clearMap(val) {
+                this.$refs.mapLayout.clearLayerSource();
+            }
         },
-      created:function(){
-        // 在页面加载时读取sessionStorage里的状态信息
-        if (sessionStorage.getItem('store')) {
-          this.$store.replaceState(
-            Object.assign(
-              {},
-              this.$store.state,
-              JSON.parse(sessionStorage.getItem('store'))
-            )
-          )
-        }
-        // 在页面刷新时将vuex里的信息保存到sessionStorage里
-        // beforeunload事件在页面刷新时先触发
-        window.addEventListener('beforeunload', () => {
-          sessionStorage.setItem('store', JSON.stringify(this.$store.state))
-        })
-
-        if(this.$store.state.name==""){
-          this.$router.push({
-            name: 'login'
-          })
-        }
-      },
-      watch: {
-        // 可以通过watch监听vuex中的text，数据变动时能够执行对应的函数
-        '$store.state.name':function(newer, older) { // 可以获取新值与老值两个参数
-          if(newer==""){
-            this.$router.push({
-              name: 'login'
+        created: function () {
+            // 在页面加载时读取sessionStorage里的状态信息
+            if (sessionStorage.getItem('store')) {
+                this.$store.replaceState(
+                    Object.assign(
+                        {},
+                        this.$store.state,
+                        JSON.parse(sessionStorage.getItem('store'))
+                    )
+                )
+            }
+            // 在页面刷新时将vuex里的信息保存到sessionStorage里
+            // beforeunload事件在页面刷新时先触发
+            window.addEventListener('beforeunload', () => {
+                sessionStorage.setItem('store', JSON.stringify(this.$store.state))
             })
-          }
+
+            if (this.$store.state.name == "") {
+                this.$router.push({
+                    name: 'login'
+                })
+            }
         },
-        deep: true // 开启深度监听
-      },
+        watch: {
+            // 可以通过watch监听vuex中的text，数据变动时能够执行对应的函数
+            '$store.state.name': function (newer, older) { // 可以获取新值与老值两个参数
+                if (newer == "") {
+                    this.$router.push({
+                        name: 'login'
+                    })
+                }
+            },
+            deep: true // 开启深度监听
+        },
     }
 </script>
 
